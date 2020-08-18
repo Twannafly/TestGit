fe097aaa24d41bce7185562297d1a1a314cff563
tianpengfei <997742685@qq.com>
2020-08-18 11:38:16 +0800

    增加ads打印

--- Repository/GitPratice/src/com/tpf/test/AdsTest.java
+++ Repository/GitPratice/src/com/tpf/test/AdsTest.java
@@ -3,6 +3,8 @@ package com.tpf.test;
 public class AdsTest {
 	public static void main(String[] args) {
 		System.out.println("this is AdsTest.java");
+		System.out.println("ads ¥Ú”°");
+		System.out.println();
 	}
 
 }
