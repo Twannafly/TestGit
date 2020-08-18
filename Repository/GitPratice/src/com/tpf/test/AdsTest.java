b4dc45fcf7a444910852394d5782031b434e9fba
tianpengfei <997742685@qq.com>
2020-08-18 11:54:37 +0800

    打印你好

--- Repository/GitPratice/src/com/tpf/test/AdsTest.java
+++ Repository/GitPratice/src/com/tpf/test/AdsTest.java
@@ -4,7 +4,7 @@ public class AdsTest {
 	public static void main(String[] args) {
 		System.out.println("this is AdsTest.java");
 		System.out.println("ads ´òÓ¡");
-		System.out.println();
+		System.out.println("ÄãºÃ");
 	}
 
 }
