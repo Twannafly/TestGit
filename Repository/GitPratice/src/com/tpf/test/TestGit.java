9fc6cd0ab04af9a59e73e04824ea39d8befb628e
tianpengfei <997742685@qq.com>
2020-08-18 09:57:40 +0800

    打印输出

--- Repository/GitPratice/src/com/tpf/test/TestGit.java
+++ Repository/GitPratice/src/com/tpf/test/TestGit.java
@@ -4,6 +4,9 @@ public class TestGit {
 	
 	
 	public static void main(String[] args) {
+		System.out.println("first test");
+		System.out.println("first test");
+		System.out.println("first test");
 		
 	}
 
