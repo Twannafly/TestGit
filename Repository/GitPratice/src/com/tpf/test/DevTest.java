ca2569f167e17a66a2196bab1959f97068a45e82
tianpengfei <997742685@qq.com>
2020-08-18 11:27:26 +0800

    ads相关提交

--- Repository/GitPratice/src/com/tpf/test/DevTest.java
+++ Repository/GitPratice/src/com/tpf/test/DevTest.java
@@ -7,7 +7,7 @@ public class DevTest {
 		System.out.println("Q");
 		System.out.println("tS");
 		System.out.println("this is devTest.java");
-		System.out.println("this is devTest.java");
+		System.out.println("这是一个ads分支上的项目");
 		System.out.println("this is devTest.java");
 	}
 
