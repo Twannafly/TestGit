355e7e00a4a0ba4c3a81ed234cd39272faf3116e
tianpengfei <tianpengfei@agree.com.cn>
2020-08-18 15:33:28 +0800

    edit AddAds

--- Repository/GitPratice/src/com/tpf/test/AddAds.java
+++ Repository/GitPratice/src/com/tpf/test/AddAds.java
@@ -3,7 +3,7 @@ package com.tpf.test;
 public class AddAds {
 	public static void main(String[] args) {
 		System.out.println("AddAds");
-		System.out.println("AddAds2");
+		System.out.println("AddAds");
 	}
 
 }
