ea27a45b46a89ea490b126ec1905db3cf8564040
Twannafly <42602026+Twannafly@users.noreply.github.com>
2020-09-09 22:41:37 +0800

    Update TestGit.java

--- Repository/GitPratice/src/com/tpf/test/TestGit.java
+++ Repository/GitPratice/src/com/tpf/test/TestGit.java
@@ -6,7 +6,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("first test");
 		System.out.println("Second test");
-		System.out.println("Third test");	
+		System.out.println("Third test12");	
 	}
 
 }
