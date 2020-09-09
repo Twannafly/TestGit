3cc7cd04b4d4748a325fa070dd3094c139bbceb9
Twannafly <42602026+Twannafly@users.noreply.github.com>
2020-09-09 22:54:39 +0800

    Update MasterTest.java
    远程更新

--- Repository/GitPratice/src/com/tpf/test/MasterTest.java
+++ Repository/GitPratice/src/com/tpf/test/MasterTest.java
@@ -3,7 +3,7 @@ package com.tpf.test;
 public class MasterTest {
 	
 	public static void main(String[] args) {
-		System.out.println("this is MasterTest.java");
+		System.out.println("this is MasterTest.javaw");
 	}
 
 }
