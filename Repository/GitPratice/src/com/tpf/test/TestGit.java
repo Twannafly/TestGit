14dcdb12b99f7bef4f9fe4ff20b19b4c1fc7da43
Twannafly <42602026+Twannafly@users.noreply.github.com>
2020-12-08 22:53:05 +0800

    直接在github上修改的

--- Repository/GitPratice/src/com/tpf/test/TestGit.java
+++ Repository/GitPratice/src/com/tpf/test/TestGit.java
@@ -7,6 +7,8 @@ public class TestGit {
 		System.out.println("first test");
 		System.out.println("Second test");
 		System.out.println("Third test12");	
+		System.out.println("这是我在github上直接修改的")
+
 	}
 
 }
